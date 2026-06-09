ee7231600cde308fe1ec6a78d04700bec18c7b42
Shubham Singh <singh.shubham0231@gmail.com>
2026-06-09 10:49:16 +0530

    Time: 11 ms (6.39%), Space: 10.8 MB (19.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
-public:
+public: 
+    bool check(string &s, int st, int end) {
+        if(st >= end) return true;
+
+        if(s[st] != s[end]) return false;
+
+        return check(s, st + 1, end - 1);
+    }
+
     bool isPalindrome(int x) {
-        string s = to_string(x);
+        /*string s = to_string(x);
 
         int st=0,end=s.size()-1;
         while(st<end){
@@ -10,7 +18,12 @@ public:
             }
             st++,end--;
         }
-        return true;
+        return true;*/
 
+
+        if(x < 0) return false;
+        string s = to_string(x);
+        return check(s, 0, s.size() - 1);
     }
+
 };
